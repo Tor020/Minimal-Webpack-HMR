f9534d428115022cbfc7594ff751fc5e045f1c79
= <=>
2018-02-11 23:20:12 -0500

    Not ready yet Hot module reload needs to be configured more check original docs

---
+++ src/main.js
@@ -0,0 +1,12 @@
+import check from './Counter'
+
+console.log('on');
+check();
+console.log('yeee')
+
+// if (module.hot) {
+//   module.hot.accept(`./Root`, () => {
+//     render(require(`./Root`).default);
+//   });
+// }
+
